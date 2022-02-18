ae4f71c6e3b84177f55d5edd342906bc548eaa17
YernarT <toktaryernar@gmail.com>
2022-02-19 04:22:48 +0600

    404 页面 完成

--- frontend/src/pages/common/PageNotFoundPage/index.jsx
+++ frontend/src/pages/common/PageNotFoundPage/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { useRecoilValue } from 'recoil';
 import { userAtom } from '@/store';
 
+import { Box, Button } from '@mui/material';
 import { LayoutTemplate } from '@/layout';
 
 import { clientLogo, deliveryLogo } from '@/assets/image';
 
 export default function PageNotFoundPage() {
 	const user = useRecoilValue(userAtom);
+	const history = useHistory();
 
 	const header = {
 		title: '404',
@@ -26,5 +30,26 @@ export default function PageNotFoundPage() {
 	}
 	header.logo = logo;
 
-	return <LayoutTemplate header={header} />;
+	return (
+		<LayoutTemplate
+			header={header}
+			content={
+				<Box
+					sx={{
+						height: '100%',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Button
+						variant="contained"
+						onClick={() => {
+							history.replace('/');
+						}}>
+						Бастапқы бетге оралу
+					</Button>
+				</Box>
+			}
+		/>
+	);
 }
