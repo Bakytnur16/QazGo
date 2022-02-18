4f282f35acbd46940ff2934ebc80d21921a3bf46
YernarT <toktaryernar@gmail.com>
2022-02-18 23:07:31 +0600

    新增一批 icon

---
+++ frontend/src/assets/icon/index.ts
@@ -0,0 +1,8 @@
+export { default as boxOpen } from './box-open.svg';
+export { default as boxClose } from './box-close.svg';
+
+export { default as search } from './search.svg';
+export { default as filter } from './filter.svg';
+
+export { default as profile } from './profile.svg';
+export { default as trusk } from './trusk.svg';
